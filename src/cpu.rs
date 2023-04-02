5a21df2ab206702eb8114642913bfc90bf7d9eb6
THISISAGOODNAME <yangyj19931231@gmail.com>
2023-04-03 00:55:03 +0800

    Emu cpu (opcodes)

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,32 @@
+use crate::opcodes;
+use std::collections::HashMap;
+
 // https://bugzmanov.github.io/nes_ebook/chapter_3.html
+
+bitflags! {
+    /// # Status Register (P) http://wiki.nesdev.com/w/index.php/Status_flags
+    ///
+    ///  7 6 5 4 3 2 1 0
+    ///  N V _ B D I Z C
+    ///  | |   | | | | +--- Carry Flag
+    ///  | |   | | | +----- Zero Flag
+    ///  | |   | | +------- Interrupt Disable
+    ///  | |   | +--------- Decimal Mode (not used on NES)
+    ///  | |   +----------- Break Command
+    ///  | +--------------- Overflow Flag
+    ///  +----------------- Negative Flag
+    ///
+    pub struct CpuFlags: u8 {
+        const CARRY             = 0b00000001;
+        const ZERO              = 0b00000010;
+        const INTERRUPT_DISABLE = 0b00000100;
+        const DECIMAL_MODE      = 0b00001000;
+        const BREAK             = 0b00010000;
+        const BREAK2            = 0b00100000;
+        const OVERFLOW          = 0b01000000;
+        const NEGATIV           = 0b10000000;
+    }
+}
 
 pub struct CPU {
     pub register_a: u8,
@@ -193,50 +221,43 @@ mod test {
     use super::*;
 
     // #[test]
-    // fn test_0xa9_lda_immidiate_load_data() {
+    // fn test_0xa9_lda_immediate_load_data() {
     //     let mut cpu = CPU::new();
-    //     cpu.interpret(vec![0xa9, 0x05, 0x00]);
-    //     assert_eq!(cpu.register_a, 0x05);
-    //     assert!(cpu.status & 0b0000_0010 == 0b00);
-    //     assert!(cpu.status & 0b1000_0000 == 0);
+    //     cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
+    //     assert_eq!(cpu.register_a, 5);
+    //     assert!(cpu.status.bits() & 0b0000_0010 == 0b00);
+    //     assert!(cpu.status.bits() & 0b1000_0000 == 0);
     // }
 
-    // #[test]
-    // fn test_0xa9_lda_zero_flag() {
-    //     let mut cpu = CPU::new();
-    //     cpu.interpret(vec![0xa9, 0x00, 0x00]);
-    //     assert!(cpu.status & 0b0000_0010 == 0b10);
-    // }
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0xaa, 0x00]);
+        cpu.reset();
+        cpu.register_a = 10;
+        cpu.run();
 
-    // #[test]
-    // fn test_0xa9_lda_negative_flag() {
-    //     let mut cpu = CPU::new();
-    //     cpu.interpret(vec![0xa9, 0xff, 0x00]);
-    //     assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
-    // }
+        assert_eq!(cpu.register_x, 10)
+    }
 
-    // #[test]
-    // fn test_0xaa_tax_move_a_to_x() {
-    //     let mut cpu = CPU::new();
-    //     cpu.register_a = 10;
-    //     cpu.interpret(vec![0xaa, 0x00]);
-    //     assert_eq!(cpu.register_x, 10);
-    // }
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 
-    // #[test]
-    // fn test_5_ops_working_together() {
-    //     let mut cpu = CPU::new();
-    //     cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
-    //     assert_eq!(cpu.register_x, 0xc1);
-    // }
-    
-    // #[test]
-    // fn test_inx_overflow() {
-    //     let mut cpu = CPU::new();
-    //     cpu.register_x = 0xff;
-    //     cpu.interpret(vec![0xe8, 0xe8, 0x00]);
-    //     assert_eq!(cpu.register_x, 1);
-    // }
+        assert_eq!(cpu.register_x, 0xc1)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0xe8, 0xe8, 0x00]);
+        cpu.reset();
+        cpu.register_x = 0xff;
+        cpu.run();
+
+        assert_eq!(cpu.register_x, 1)
+    }
 
     #[test]
     fn test_lda_from_memory() {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 pub mod cpu;
+pub mod opcodes;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate bitflags;
 
 fn main() {
     println!("Hello, world!");
